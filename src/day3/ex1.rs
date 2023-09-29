a17b026268379ca0203e11527343d3ae50f140a0
Jaco Esterhuizen <jaco@litriv.com>
2023-09-29 15:28:46 +0200

    Call `priority_sum` from `solve`

--- src/day3/ex1.rs
+++ src/day3/ex1.rs
@@ -1,11 +1,13 @@
+use crate::lines_for;
+
 type Priority = u8;
 type PrioritySum = i32;
 
 fn solve() -> PrioritySum {
-    priority_sum()
+    priority_sum(lines_for("input_data/1"))
 }
 
-fn priority_sum() -> PrioritySum {
+fn priority_sum(vals: impl IntoIterator<Item = String>) -> PrioritySum {
     1000
 }
 
