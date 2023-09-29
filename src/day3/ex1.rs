f7e73781862a7847e1bc799ef437e709a60f5f9a
Jaco Esterhuizen <jaco@litriv.com>
2023-09-29 15:38:37 +0200

    Add `priority` and let `priority_sum` call it
    `priority_sum` now iterates over the input and calls `priority` on each
    line, summing the results.

--- src/day3/ex1.rs
+++ src/day3/ex1.rs
@@ -1,14 +1,20 @@
 use crate::lines_for;
 
 type Priority = u8;
-type PrioritySum = i32;
+type PrioritySum = u32;
 
 fn solve() -> PrioritySum {
     priority_sum(lines_for("input_data/1"))
 }
 
 fn priority_sum(vals: impl IntoIterator<Item = String>) -> PrioritySum {
-    1000
+    vals.into_iter()
+        .map(|l| <u8 as Into<u32>>::into(priority(l)))
+        .sum()
+}
+
+fn priority(l: String) -> Priority {
+    10
 }
 
 #[cfg(test)]
